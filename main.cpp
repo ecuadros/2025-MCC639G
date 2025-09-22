63a80f54e81f1cd2742499aed76c6a028df71206
quispedavid <dquispeca@unsa.edu.pe>
2025-09-22 17:09:31 -0500

    Modifica test y main, David Quispe

--- main.cpp
+++ main.cpp
@@ -16,12 +16,15 @@ using namespace std;
 
 int main(){
     cout << "Hello EDA - MCC639G" <<endl;
-    int t = prueba(5);
+    /*int t = prueba(5);
     cout << "Respuesta:" << t << endl;
 
     // NUEVO: usando la función suma, Luis Selis
     int resultado = suma(3, 7);
-    cout << "Suma de 3 + 7 = " << resultado << endl;
+    cout << "Suma de 3 + 7 = " << resultado << endl;*/
 
+
+	// NUEVO: funcion suma, David Quispe
+	cout << "Suma de 5 y 4 es: " << sum(4,5) << endl;
     return 0;
 }
--- test.h
+++ test.h
@@ -4,4 +4,8 @@
 int prueba(int n);
 int suma(int a, int b);     // nueva función Luis Selis
 
+
+int sum(int x, int y){ //nueva funcion David Quispe
+	return x + y;
+};
 #endif
