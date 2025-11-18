0275be4c30e83643908acff35e732d0be2b144d4
Oscar Toledo <tgoscar@gmail.com>
2025-11-17 21:11:55 -0500

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -70,7 +70,7 @@ public:
 
     // TODO: Move contructor
     CLinkedList(CLinkedList &&other)  {
-        std::scoped_lock lock(other.m_mutex);
+        std::scoped_lock lock(m_mutex, other.m_mutex);
         m_pHead = std::exchange(other.m_pHead, nullptr); //usar exchange
         m_size = std::exchange(other.m_size, 0);
 
